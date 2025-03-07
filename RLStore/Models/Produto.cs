6b2e1b89c6a156f4b74b5e5576f2e544caff70c5
RaissaBorgess <raissasantos123.rs33@gmail.com>
2025-03-07 10:44:19 -0300

    adicionando dados mockados

--- RLStore/Models/Produto.cs
+++ RLStore/Models/Produto.cs
@@ -36,4 +36,8 @@ namespace RLStore.Models;
         [Range(0, double.MaxValue)]
         [Column(TypeName = "numeric(10,2)")]
         public decimal ValorVenda { get; set; }
+
+        public bool Destaque { get; set; }
+
+        public List<ProdutoFoto> Fotos { get; set; }
     }
