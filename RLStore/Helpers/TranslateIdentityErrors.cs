c732216b09e8de1f73ad99da6efd7ccfa6c4dbb9
RaissaBorgess <raissasantos123.rs33@gmail.com>
2025-05-06 12:27:27 -0300

    ARRUMAR O ERRO

--- RLStore/Helpers/TranslateIdentityErrors.cs
+++ RLStore/Helpers/TranslateIdentityErrors.cs
@@ -12,11 +12,22 @@ public static class TranslateIndentityErrors
             "LoginAlreadyAssociated" => "Já existe um usuário com este login.", 
             "InvalidUserName" => $"Este login é inválido, um login deve conter apenas letras ou dígitos.",
             "InvalidEmail" => "Email inválido.", 
+            "DuplicateUserName" => "Este login já está sendo utilizado.",
+            "DuplicateEmail" => "Este email já está sendo utilizado.",
+            "InavlidRoleName" => "Esta permissão pppe invalida.",
+            "DuplicateRoleName" => "Esta permissão já está sendo utilizada.",
+            "UserAlreadyInRole" => "Usuário já possui esta permissão.",
+            "UserNotInRole" => "Usuário não tem permissão.",
+            "UserLockoutNotEnabled" => "Lockout não está habilitado para este usuário.",
+            "UserALreadyHasPassword" => "Usuário não tem permissão",
+            "PasswordMismatch" => "Senha incorreta.",
+            "PasswordTooShort" => "Senha muito curta.",
+            "PasswordRequiresNonAlphanumeric" => "Senhas devem conter ao menos um caracter não alfanumérico.",
+            "PasswordRequiresDigit" => "Senhas devem conter ao menos um digito ('0'-'9).",
+            "PasswordRequiresLower" => "Senhas devem conter ao menos um caracter em caixa baixa ('a'-'z').",
+            "PasswordRequiresUpper" => "senhas devem conter ao menos um caracter em caixa alta ('A'-'Z').",
+            _=> "Ocorreu um erro desconhecido.",
         };
     return message;
-    
     }
-    
 }
-
-
